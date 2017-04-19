55f331fc5d400c25c78caf35b09f77fb85e387d8
karolinspang <karolin.spang@gmail.com>
2017-04-19 11:26:28 +0200

    ta bort testing

--- Eatimize/js/init.js
+++ Eatimize/js/init.js
@@ -3,9 +3,6 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-      
-      console.log("Testning");
-
-  console.log("Testing");
+    
   }); // end of document ready
 })(jQuery); // end of jQuery name space
