2b6fbc9e2f8d76ac19a0ab78e037c61b196307a5
Ludwig <ludwigninn@hotmail.com>
2017-05-09 11:41:55 +0200

    Webbserver ändringar
    Ändrat namn så att filerna funkar i webbserver

--- Eatimize/js/init.js
+++ Eatimize/js/init.js
@@ -1,13 +1,13 @@
 (function($){
   $(function(){
-      
+
        $(document).ready(function(){
-                
-                //detta fungerar bara om man kör från localhost eller i annan webbläsare än chrome: 
-                $('#navbar').load('navbar.tpl');         
+
+                //detta fungerar bara om man kör från localhost eller i annan webbläsare än chrome:
+                $('#navbar').load('navbar.php');
 
             }); 
-            
+
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
