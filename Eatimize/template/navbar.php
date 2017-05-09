9dbe1ae3863333585eb22ece7805f12043e64311
Ludwig <ludwigninn@hotmail.com>
2017-05-09 12:41:41 +0200

    Logga in

--- Eatimize/template/navbar.php
+++ Eatimize/template/navbar.php
@@ -20,7 +20,7 @@
         <li id="searchBtn"><a>Sök<i class="material-icons left">search</i></a></li>
         <li><a href="EATIMIZE.html">Info</a></li>
         <li><a href="Profile.html">Min sida</a></li>
-        <li><a href="Recept.html">Logga ut</a></li>
+        <li><a href="login&register.html">Logga in</a></li>
       </ul>
          <ul class="side-nav" id="mobile-demo">
      <!--   <li><a href="Recept.html">Sök</a></li> -->
