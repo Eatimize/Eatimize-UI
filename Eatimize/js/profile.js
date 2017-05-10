f1d794c74bb17d944a2f6cd167d1e61639ddb078
emelinpettersson <emelix.pettersson@gmail.com>
2017-05-10 22:41:11 +0200

    Ändring av infofält och spara fungerar
    Fungerar att ändra, avbryta ändring och spara ändring. Ändringen puttas
    till sessionstorage när man sparat.

--- Eatimize/js/profile.js
+++ Eatimize/js/profile.js
@@ -1,9 +1,11 @@
 	var  activity = sessionStorage.getItem("actvity");
-	var  sex =  sessionStorage.getItem("sex");
+	var  sex = sessionStorage.getItem("sex");
 	var  weight = sessionStorage.getItem("weight");
-	var  height =  sessionStorage.getItem("height");
+	var  height = sessionStorage.getItem("height");
 	var  goal = sessionStorage.getItem("goal");
 	var  age = sessionStorage.getItem("age");
+	
+	var editMode = false;
 	
 function getInfo() {
 	$("#activity").val(activity);
@@ -12,10 +14,105 @@ function getInfo() {
 	$("#height").val(height);
 	$("#goal").val(goal);
 	$("#age").val(age);
-
+	
+	$("#sex").material_select();
+	$("#activity").material_select();
+	$("#goal").material_select();
 };
 
+// Förmodligen tas bort
+/*var activityLevels = [];
+	activityLevels["1.2"] = "Lite eller ingen motion alls/kontorsarbete.";
+	activityLevels["1.3"] = "Lätt motion 1-3 gånger i veckan.";
+	activityLevels["1.5"] = "Medel motion 3-5 gånger i veckan/fysiskt arbete.";
+	activityLevels["1.7"] = "Tung motion 6-7gånger i veckan.";
+	activityLevels["1.9"] = "Mycket tung motion 6-7 gånger i veckan.";
+	activityLevels["2.0"] = "Elitidrottare under högsäsong.";
+	
+var goals = [];
+	goals["muskler"] =  "Att bygga muskler.";
+	goals["vikt"] =  "Att gå ner i vikt.";
+	goals["hälsosamt"] =  "Att äta hälsosamt.";
+	
+var sexArray = [];
+	sexArray["Female"] = "Kvinna";
+	sexArray["Male"] = "Man";*/
+
+function editInformation(){
+
+	if(editMode) {
+		 enableForm();
+		 editMode = false;
+	}else{
+		restoreInformation();
+		editMode = true;
+	}
+}
+
+function enableForm() {
+	$("#edit").text("Avbryt");
+	
+	$("#activity").prop("disabled", false);
+	$("#sex").prop("disabled", false);
+	$("#weight").prop("disabled", false);
+	$("#height").prop("disabled", false);
+	$("#goal").prop("disabled", false);
+	$("#age").prop("disabled", false);
+	
+	$("#sex").material_select();
+	$("#activity").material_select();
+	$("#goal").material_select();	
+}
+
+function restoreInformation(){
+	$("#edit").text("Ändra information");
+	
+	$("#activity").prop("disabled", true);
+	$("#sex").prop("disabled", true);
+	$("#weight").prop("disabled", true);
+	$("#height").prop("disabled", true);
+	$("#goal").prop("disabled", true);
+	$("#age").prop("disabled", true);
+	
+	$("#sex").material_select();
+	$("#activity").material_select();
+	$("#goal").material_select();
+	
+	getInfo();
+}
+
+function saveChanges(){
+	var  activity = $("#activity").val();
+	var  sex = $("#sex").val();
+	var  weight = $("#weight").val();
+	var  height = $("#height").val();
+	var  goal = $("#goal").val();
+	var  age = $("#age").val();
+	
+	sessionStorage.setItem("activity", activity);
+	sessionStorage.setItem("sex", sex);
+	sessionStorage.setItem("weight", weight);
+	sessionStorage.setItem("height", height);
+	sessionStorage.setItem("goal", goal);
+	sessionStorage.setItem("age", age);
+	
+	$("#activity").prop("disabled", true);
+	$("#sex").prop("disabled", true);
+	$("#weight").prop("disabled", true);
+	$("#height").prop("disabled", true);
+	$("#goal").prop("disabled", true);
+	$("#age").prop("disabled", true);
+	
+	$("#sex").material_select();
+	$("#activity").material_select();
+	$("#goal").material_select();
+		
+}
+	
 $(document).ready(function () {
-   getInfo();
+	$('select').material_select();
+    getInfo();
 });
+
+
 
