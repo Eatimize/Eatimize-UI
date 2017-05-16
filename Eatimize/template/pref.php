407b1f11a0f7618a2eb985a046888b588451092e
karolinspang <karolin.spang@gmail.com>
2017-05-16 11:14:30 +0200

    Uppdaterat pref så att det stämmer överens med andra branches

--- Eatimize/template/pref.php
+++ Eatimize/template/pref.php
@@ -1,21 +1,27 @@
 <html>
     <head>
-        <link href="/css/pref.css" rel="stylesheet" type="text/css">    
-    
-        <script>
-          $('.cb').click(function() {
-              
-            sessionStorage.setItem($(this).attr("value"), $(this).is(":checked"));
+        <link href="css/pref.css" rel="stylesheet" type="text/css">    
+  <script>
+        
+          $(document).ready(function(){
+                
+            $('.cb').click(function() {
+                
+            /*    console.log($(this).attr("value") + ", " + $(this).is(":checked"));  
+            */                      sessionStorage.setItem($(this).attr("value"), $(this).is(":checked"));
                                     
             });
-        </script>
+        
+        });
+    </script>
+    
     </head>
 
     <body>
         <div class="prefDiv">
             <input type='checkbox' name='pref' value='gluten' id="gluten" class="cb"/>
             <label for="gluten">
-                <img src="../icons/gluten.png" class="prefimg">
+                <img src="icons/gluten.png" class="prefimg">
                 <span>Glutenallergi</span>
             </label>    
 
@@ -25,7 +31,7 @@
         <div class="prefDiv">
             <input type='checkbox' name='pref' value='milkprotein' id="milkprotein" class="cb"/>    
                 <label for="milkprotein">
-                    <img src="../icons/milkprotein.png" class="prefimg">
+                    <img src="icons/milkprotein.png" class="prefimg">
                     <span>Mjölkprotein-allergi</span>
                 </label>
         </div>       
@@ -34,7 +40,7 @@
         <div class="prefDiv">
             <input type='checkbox' name='pref' value='laktos' id="laktos" class="cb"/>    
                 <label for="laktos">
-                    <img src="../icons/laktos.png" class="prefimg">
+                    <img src="icons/laktos.png" class="prefimg">
                     <span>Laktosintolerant</span>
                 </label>
          </div>  
@@ -42,7 +48,7 @@
         <div class="prefDiv">        
             <input type='checkbox' name='pref' value='nut' id="nut" class="cb"/>    
                 <label for="nut">
-                    <img src="../icons/nut.png" class="prefimg">
+                    <img src="icons/nut.png" class="prefimg">
                     <span>Jordnötsallergi</span>
                 </label>        
         </div>  
@@ -50,7 +56,7 @@
         <div class="prefDiv">
             <input type='checkbox' name='pref' value='vegetarian' id="vegetarian" class="cb"/>    
                 <label for="vegetarian">
-                    <img src="../icons/vegetarian.png"  class="prefimg">
+                    <img src="icons/vegetarian.png"  class="prefimg">
                     <span>Vegetarian</span>
                 </label>      
         </div>  
@@ -58,11 +64,11 @@
         <div class="prefDiv">
             <input type='checkbox' name='pref' value='vegan' id="vegan" class="cb"/>    
                 <label for="vegan">
-                    <img src="../icons/vegan.png" class="prefimg">
+                    <img src="icons/vegan.png" class="prefimg">
                     <span>Vegan</span>
                 </label>
         </div>
-
+    
     </body>
     
 </html>  
