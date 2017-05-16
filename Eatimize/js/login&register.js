9aa9d4b5d42d24e05d0415768337fcbf26743956
MaNcO5 <marjansherdenkovski@gmail.com>
2017-05-16 23:22:37 +0200

    uppdate

--- Eatimize/js/login&register.js
+++ Eatimize/js/login&register.js
@@ -1,28 +1,43 @@
-var cont = 0;
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+  var $this = $(this),
+      label = $this.prev('label');
 
-function register(){
+	  if (e.type === 'keyup') {
+			if ($this.val() === '') {
+          label.removeClass('active highlight');
+        } else {
+          label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+    	if( $this.val() === '' ) {
+    		label.removeClass('active highlight'); 
+			} else {
+		    label.removeClass('highlight');   
+			}   
+    } else if (e.type === 'focus') {
+      
+      if( $this.val() === '' ) {
+    		label.removeClass('highlight'); 
+			} 
+      else if( $this.val() !== '' ) {
+		    label.addClass('highlight');
+			}
+    }
 
-     cont++;
-		
-		if(cont==1){
-		 	$('.box').animate({height:'695px'}, 550);
-			$('.show').css('display','block');
-			$('#logintoregister').text('Registrera dig');
-			$('#buttonlogintoregister').text('Registrera dig');
-			$('#plogintoregister').text("Är du redan registrerad?");
-			$('#textchange').text('Logga in');
-		}
-		else
-		{
-			$('.show').css('display','none');
-			$('.box').animate({height:'365px'}, 550);
-			$('#logintoregister').text('Login');
-			$('#buttonlogintoregister').text('Login');
-			$('#plogintoregister').text("Inte medlem?");
-			$('#textchange').text('Register');
-			cont = 0;
-		}
-}
-function logIn(){
-            document.location.href = "EATIMIZE.html";
-}
+});
+
+$('.tab a').on('click', function (e) {
+  
+  e.preventDefault();
+  
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+  
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+  
+  $(target).fadeIn(600);
+  
+});
